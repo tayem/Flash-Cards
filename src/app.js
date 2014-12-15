72bcb7642353986fd7fbb4ca8e71332dc650c4a4
tayem <abdeltayem@gmail.com>
2014-12-15 10:31:53 -0700

    Added words and definitions.
    We added two lists, one of words and another one that is parallel to the first containing the definitions of the words.

--- src/app.js
+++ src/app.js
@@ -6,49 +6,44 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
-
+var words = ["Jacobins",
+			 "Sans-culottes",
+			 "Girondins",
+			 "Marat",
+			 "Danton",
+			 "National Convention",
+			 "September Massacres",
+			 "Tuiliries",
+			 "Louis XVI",
+			 "Robespierre"];
+var definitions = ["Extremist group, the Montagnards.",
+			"Mass Mobs who wanted extreme change.",
+			"Moderate Republicans.",
+			"Extremist doctor; part of the Jacobins.",
+		    "Man of the people, one of the leaders of the Jacobins and an extremist as well.",
+			"The group of individuals assigned power over the government of the people.",
+			"The killing of monarchists, the public and sans-culottes went to the Tuiliries and killed all the opposition.",
+			"The palace where Louis was kept after the Flight to Varennes.",
+			"The previous monarch ruling France who is executed to show the death of the constitutional monarchy.",
+			"The most extreme member of the Jacobins, started the Reign of Terror and wanted complete power."];
 var main = new UI.Card({
-  title: 'Pebble.js',
+  title: 'Flashcards',
   icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  subtitle: 'The best way to study!',
+  body: 'Press select button to go to menu.'
 });
+
 
 main.show();
 
 main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
+
 });
 
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
 
+});
+	
 main.on('click', 'down', function(e) {
   var card = new UI.Card();
   card.title('A Card');
